06e134e3806f80d1cbbe4c7a4cc41d40f6c87adc
ramsshukla <139951978+ramsshukla@users.noreply.github.com>
2025-09-22 14:35:04 +0000

    Datatypes

---
+++ 01_basics/03_COnversionOperations.js
@@ -0,0 +1,17 @@
+let marks = "100"
+
+// console.log(typeof marks);
+// console.log(typeof(marks));
+
+
+let marksinNumber = Number(marks)
+// console.log(typeof marksinNumber);
+
+let isloggedIn = 
+let BooleanisloggedIn = Boolean(isloggedIn)
+
+console.log(BooleanisloggedIn);
+
+//1 => true, 0=> false
+// "" => false
+// "abc" => true
---
+++ 01_var.js
@@ -0,0 +1,10 @@
+const accountId = 112233
+let email = "ram@google.co"
+var accountCity = "Ambala"
+accountPassword = "123456"
+
+email = "ram@yahoo.com"
+accountCity = "Mohali"
+accountPassword = "111111"
+console.table([accountId, email, accountCity, accountPassword]);
+
