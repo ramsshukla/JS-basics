461335dd785ef061c800e7d79a1de1dd312dbfeb
ramsshukla <139951978+ramsshukla@users.noreply.github.com>
2025-10-26 17:20:54 +0000

    nums_and_math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+ let marks = 400;
+
+ let min_marks = new Number(100);
+
+//  console.log(marks);
+//  console.log(min_marks.toFixed(2));
+
+ let other_num = 123.8774;
+//  console.log(other_num.toPrecision(5));
+
+ const nums = 10000000;
+
+//  console.log(nums.toLocaleString());
+//  console.log(nums.toLocaleString('en-IN'));
+ 
+//  ********************************* MATHS ****************************
+
+// console.log(Math.abs(4));
+// console.log(Math.abs(-1));
+// console.log(Math.round(5.3));
+// console.log(Math.ceil(5.3));
+// console.log(Math.floor(5.3));
+// console.log(Math.min(5, 3, 7, 2));
+// console.log(Math.max(5, 3, 7, 2));
+// console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+
+const max = 20;
+const min = 10;
+
+// console.log(Math.floor((Math.random(max, min))));  =>>>> xxx
+// console.log(Math.floor((Math.random() * (max - min + 1)) + min)); =>>>>> Correct
