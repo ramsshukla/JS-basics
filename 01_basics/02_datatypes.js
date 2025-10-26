c292a58f432faf9514042b00100cd458ff268a47
ramsshukla <139951978+ramsshukla@users.noreply.github.com>
2025-10-26 09:35:45 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" // treats all JS code as newer versions.
+
+
+// Data Types =>
+// 1. Primitive Datatypes:
+// string
+// number
+// bigint
+// boolean
+// null => This is a standalone value.
+// undefined 
+// symbol => Unique
+
+// 2. Non-Primitive Datatypes
+
+// object 
+
+// typeof null => Object
+// while typeof undefined => undefined. 
