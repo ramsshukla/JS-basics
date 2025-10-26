642cc05f866fc83b1d4bc24e295661850e222264
ramsshukla <139951978+ramsshukla@users.noreply.github.com>
2025-10-26 10:52:36 +0000

    COnversionOPerations

--- 01_basics/03_COnversionOperations.js
+++ 01_basics/03_COnversionOperations.js
@@ -1,3 +1,5 @@
+const { useDebugValue } = require("react");
+
 let marks = "100"
 
 // console.log(typeof marks);
@@ -7,11 +9,36 @@ let marks = "100"
 let marksinNumber = Number(marks)
 // console.log(typeof marksinNumber);
 
-let isloggedIn = 
+let isloggedIn = true
 let BooleanisloggedIn = Boolean(isloggedIn)
 
-console.log(BooleanisloggedIn);
+// console.log(BooleanisloggedIn);
 
+// "22" => 22
+//  "22abc" => NaN
 //1 => true, 0=> false
 // "" => false
 // "abc" => true
+
+// ******************************Operations ************************
+
+let val = 3;
+let negval = -val;
+// console.log(negval);
+
+// console.log("1" + 3 + 2); 132
+// console.log(1 + 3 + "2"); 42
+// Converts rest of the part according to the first data type assigned.
+
+
+let a = 3;
+let b = a++;
+
+console.log(`a: ${a}, b: ${b}`);
+let x = 3;
+let y = ++x;
+
+console.log(`x: ${x}, y: ${y}`);
+
+//  ********* Prefix increments value before usage.
+//  ********* Postfix increments value after usage.
